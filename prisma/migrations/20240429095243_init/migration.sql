065c8e6af436a1dbe3633c6c5857cc7a5b06b389
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-29 14:53:09 +0500

    psg

---
+++ prisma/migrations/20240429095243_init/migration.sql
@@ -0,0 +1,125 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "imageUrl" TEXT,
+    "price" DOUBLE PRECISION,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "categoryId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attachment" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "courseId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chapter" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "videoUrl" TEXT,
+    "position" INTEGER NOT NULL,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "isFree" BOOLEAN NOT NULL DEFAULT false,
+    "courseId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chapter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MuxData" (
+    "id" TEXT NOT NULL,
+    "assetId" TEXT NOT NULL,
+    "playbackId" TEXT,
+    "chapterId" TEXT,
+
+    CONSTRAINT "MuxData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserProgress" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "isCompleted" BOOLEAN NOT NULL,
+    "chapterId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserProgress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Purchase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "chapterId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Purchase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StripeCustomer" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StripeCustomer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE INDEX "Course_categoryId_idx" ON "Course"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "Attachment_courseId_idx" ON "Attachment"("courseId");
+
+-- CreateIndex
+CREATE INDEX "Chapter_courseId_idx" ON "Chapter"("courseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MuxData_chapterId_key" ON "MuxData"("chapterId");
+
+-- CreateIndex
+CREATE INDEX "UserProgress_chapterId_idx" ON "UserProgress"("chapterId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProgress_userId_chapterId_key" ON "UserProgress"("userId", "chapterId");
+
+-- CreateIndex
+CREATE INDEX "Purchase_chapterId_idx" ON "Purchase"("chapterId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StripeCustomer_userId_key" ON "StripeCustomer"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StripeCustomer_customerId_key" ON "StripeCustomer"("customerId");
